14dc40de8936ece24dccc995107edb6cddc43a32
Nathan <abegaznathan@gmail.com>
2021-05-18 01:25:11 -0700

    Added ActionBar arrow to LoginActivity

--- app/src/main/java/com/example/noteshare/LoginActivity.java
+++ app/src/main/java/com/example/noteshare/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,10 +26,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        if(ParseUser.getCurrentUser() != null){
-            goMainActivity();
-        }
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
@@ -63,5 +62,11 @@ public class LoginActivity extends AppCompatActivity {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         finish();
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), WelcomeActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
     }
 }
